853c434dc9f778030cf0cdcef896559d8157d956
DaviCarvalhoo <davicarvalhotech@gmail.com>
2023-06-01 22:49:48 -0300

    create list in python

---
+++ day_1/dicts.py
@@ -0,0 +1,5 @@
+aluno={"nome":"Davi","idade":19,"fav_food":"pizza"}
+aluno["score"] = 200
+print(aluno)
+alunos = [{"nome":"Davi","idade":19,"fav_food":"pizza"},{"nome":"Daniel","idade":12,"fav_food":"pizza"},{"nome":"Amanda","idade":35,"fav_food":"pizza"}]
+print(alunos[0])
---
+++ day_1/list_and_tuples.py
@@ -0,0 +1,13 @@
+#criar lista
+alunos = ["Davi", "Daniel"]
+#adicionar a lista
+alunos.append("Fernando")
+#adicionar com variavel
+new_aluno = "Sara"
+alunos.append(new_aluno)
+#adicionar ao inicio
+alunos.insert(0,"Tati")
+#print fim da lista
+print(alunos[-1])
+#print lista
+print(alunos)
