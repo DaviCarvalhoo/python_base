26adb7cd3a2c1eff4eb7fcf5305ddc587ab18809
DaviCarvalhoo <davicarvalhotech@gmail.com>
2023-06-15 19:45:03 -0300

    add changes in file

--- day_1/list_and_tuples.py
+++ day_1/list_and_tuples.py
@@ -6,8 +6,10 @@ alunos.append("Fernando")
 new_aluno = "Sara"
 alunos.append(new_aluno)
 #adicionar ao inicio
-alunos.insert(0,"Tati")
+alunos.insert(0,"Paulo")
 #print fim da lista
 print(alunos[-1])
 #print lista
 print(alunos)
+#dias da semana em tupla
+semana = ("domingo", "segunda", "terça", "quarta")#...
